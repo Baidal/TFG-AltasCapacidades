51072559d310d0fafb775a34f7b242d4ac2cb77a
Baidal <lvr31@alu.ua.es>
2022-06-21 14:02:40 +0200

    Añadido componente input

---
+++ client/src/components/InputBuscar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex space-x-2 mx-auto border-2 border-black p-1 rounded-lg w-2/3">
+        <SearchIcon class="w-6 h-6" />
+        <input
+            type="search"
+            id="users"
+            :placeholder="placeHolder"
+            class="w-full focus:outline-none"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+        />
+    </div>
+</template>
+
+<script>
+import {SearchIcon} from '@heroicons/vue/solid'
+
+export default {
+    name: 'InputBuscar',
+    components: {
+        SearchIcon
+    },
+    props: ['modelValue', 'placeHolder'],
+    emits: ['update:modelValue']
+}
+</script>
