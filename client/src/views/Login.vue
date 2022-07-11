525b0bf8ac35bf05e5f090ebccac07cddad1772d
Baidal <lvr31@alu.ua.es>
2022-07-11 17:04:45 +0200

    Mejorado css de login

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mx-auto w-1/4 mt-10 border-l-2 border-gray-900 rounded-md shadow-xl p-6 text-center flex flex-col items-center">
+    <div class="mx-auto w-1/4 mt-10 border-l-2 border-indigo-600 rounded-md shadow-xl p-6 text-center flex flex-col items-center">
         <h1 class="font-bold text-lg">Iniciar sesión</h1>
         <input class="mt-3 border-2 border-gray-700 rounded-lg w-2/3" placeholder="Email..."/>
         <input class="mt-3 border-2 border-gray-700 rounded-lg w-2/3" placeholder="Contraseña..."/>
