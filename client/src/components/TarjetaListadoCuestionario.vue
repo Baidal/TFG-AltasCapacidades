ea81452d312224cf2b493a3629a7c64123bf9fae
Baidal <lvr31@alu.ua.es>
2022-12-27 17:43:37 +0100

    Modificado css de cuestionarios

--- client/src/components/TarjetaListadoCuestionario.vue
+++ client/src/components/TarjetaListadoCuestionario.vue
@@ -1,5 +1,5 @@
 <template>
-    <div  class="w-full h-56 flex justify-center text-center p-2 relative">
+    <div  class="w-full h-40 flex justify-center text-center p-2 relative">
         <div class="absolute top-4 right-4 cursor-pointer"><XCircleIcon class="w-8 h-8 z-10" @click="$emit('eliminarCuestionario', this.cuestionario.id)" v-if="mostrarEliminar"/></div>
         <div @click="this.$router.push({name: 'Cuestionario', params: {id: cuestionario?.id}})" class="rounded-lg shadow-lg w-full mx-1 flex flex-col items-center hover:shadow-xl cursor-pointer">
             <ClipboardListIcon class="h-22 text-gray-800"/>
